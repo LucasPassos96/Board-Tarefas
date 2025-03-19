ce311c1eb697e4418e84462f7bb10f5d11941d66
LucasBinho <lucas_passos@msn.com>
2025-03-18 21:23:59 -0300

    funcionalidade de renderizar numeros reais de quantidade posts e comentarios em pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import styles from "@/styles/home.module.css";
 import Image from 'next/image';
 import heroImg from '../../public/assets/hero.png'
 
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from "../services/firebaseConnection";
 
-export default function Home() {
+interface HomeProps {
+  posts: number;
+  comments: number;
+}
+
+export default function Home({ posts, comments }: HomeProps) {
+
   return (
     <div
     className={styles.container} 
@@ -29,15 +38,33 @@ export default function Home() {
 
             <div className={styles.infoContent}>
                 <section className={styles.box}>
-                  +12 posts
+                  +{posts} posts
                 </section>
                 <section className={styles.box}>
-                  +90 comentários
+                  +{comments} comentários
                 </section>
             </div>
           </div>
         </main>
       </div>
- 
   );
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  
+  // buscar do banco de forma estatica os numeros de tasks/comentarios, e mostrar no componente
+  const commentRef = collection(db, "comentarios");
+  const postRef = collection(db, "tarefas");
+  
+  const commentSnapshot = await getDocs(commentRef);
+  const postSnapshot = await getDocs(postRef);
+  
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0
+    },
+    revalidate: 90 //revalidado a cada 90 segundos
+  }
 }
