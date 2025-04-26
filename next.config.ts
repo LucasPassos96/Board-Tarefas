d0ce88eec95adcacd63bfaf7e2d96c5a605e74b6
LucasBinho <lucas_passos@msn.com>
2025-04-26 12:01:06 -0300

    ajustando arquivo next config para deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,21 @@
 import type { NextConfig } from "next";
+import path from "path";
+
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  webpack(config, { isServer }) {
+  webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = { fs: false };
     }
+    
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+      '@styles': path.resolve(__dirname, 'src/styles')
+    };
+    
     return config;
   },
 };
